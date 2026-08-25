777d530a591368f5080af9f236dd3a137f8acab5
mirzayv <mirzayv919@gmail.com>
2026-08-25 14:09:36 +0900

    fix: modify updateMember GraphQL api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -6,6 +6,8 @@ import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
+import { ObjectId } from 'bson';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Injectable()
 export class MemberService {
@@ -46,8 +48,21 @@ export class MemberService {
 		return response;
 	}
 
-	public async updateMember(): Promise<string> {
-		return 'updateMember executed!';
+	public async updateMember(memberId: ObjectId, input: MemberUpdate): Promise<Member> {
+		const result: Member | null = await this.memberModel
+			.findOneAndUpdate(
+				{
+					_id: memberId,
+					memberStatus: MemberStatus.ACTIVE,
+				},
+				input,
+				{ new: true },
+			)
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.UPLOAD_FAILED);
+
+		result.accessToken = await this.authService.createToken(result);
+		return result;
 	}
 
 	public async getMember(): Promise<string> {
